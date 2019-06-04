e7ea4bc00ae82b4c28076896cdbd50dab3adf031
snjylin <snjy.lin@gmail.com>
2019-06-04 12:14:28 +0800

    update

---
+++ movie/quotes.js
@@ -0,0 +1,6 @@
+var callback = [
+  {"content":"明天又是崭新的一天。","origin_content":"Tomorrow is another day.", "title":"乱世佳人", "origin_title": "Gone with the Wind (1939)"},
+  {"content":"人，得自个儿成全自个儿。","origin_content":"", "title":"霸王别姬", "origin_title": "霸王别姬"},
+  {"content":"如果将梦想作为信仰,不放弃地追求下去,一定会梦想成真的！","origin_content":"", "title":"火影忍者剧场版1：拯救雪国公主", "origin_title": "Naruto Movie 1: Daikatsugeki! Yukihime ninpôchô dattebayo!!"},
+  {"content":"我们是该去什么地方，还是要在这片荒地上寻求更好的自己？","origin_content":"Where must we go......We who wander this Wasteland in search of our better selves? ", "title":"疯狂的麦克斯：狂暴之路 (2015)", "origin_title": "Mad Max: Fury Road"},
+];
